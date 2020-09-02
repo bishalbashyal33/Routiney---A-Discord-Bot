7f3fa6d756fc149dd9f24119764c42f3351d8eb5
Bishal Bashyal <63231700+bishalbashyal33@users.noreply.github.com>
2020-09-02 19:23:16 +0545

    Updated

--- utils/mongoose.js
+++ utils/mongoose.js
@@ -16,7 +16,7 @@ module.exports = {
         mongoose.connect(`mongodb+srv://admin:<${process.env.pass}>@cluster0.sac2a.mongodb.net/<dbname>?retryWrites=true&w=majority`, dbOptions);
         mongoose.set('useFindAndModify', false);
         mongoose.Promise = global.Promise;
-
+//
         mongoose.connection.on('connected', () => {
             console.log('Mongoose has successfully connected!');
         });
